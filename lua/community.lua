1e0d732ecd0c40a44e894c35ac778b413870bba4
emgixiii <emgi@live.in>
2024-05-19 14:20:03 +0530

    Setup Catppuccin, WIP migrations to v4

---
+++ lua/community.lua
@@ -0,0 +1,30 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+    "AstroNvim/astrocommunity",
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.media.image-nvim" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.editing-support.wildfire-nvim" },
+  { import = "astrocommunity.lsp.lsp-lens-nvim" },
+  -- { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.editing-support.hypersonic-nvim" },
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
+}
---
+++ lua/plugins/astroui.lua
@@ -0,0 +1,43 @@
+-- AstroUI provides the basis for configuring the AstroNvim User Interface
+-- Configuration documentation can be found with `:h astroui`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
+
+---@type LazySpec
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    -- change colorscheme
+    colorscheme = "catppuccin",
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    highlights = {
+      init = { -- this table overrides highlights in all themes
+        Normal = { bg = NONE, guibg = NONE, ctermbg = NONE },
+        NormalNC = { bg = NONE, guibg = NONE, ctermbg = NONE },
+        NeoTreeNormal = { bg = NONE, guibg = NONE, ctermbg = NONE },
+        NormalFloat = { bg = NONE, guibg = NONE, ctermbg = NONE },
+        NeoTreeNormalNC = { bg = NONE, guibg = NONE, ctermbg = NONE },
+        NotifyBackground = { bg = "#000000" },
+        NonText = { bg = NONE, guibg = NONE, ctermbg = NONE },
+      },
+      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+        -- Normal = { bg = NONE },
+      },
+    },
+    -- Icons can be configured throughout the interface
+    icons = {
+      -- configure the loading of the lsp in the status line
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+    },
+  },
+}
