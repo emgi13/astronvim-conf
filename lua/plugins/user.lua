2fe9397ec8b7c92c41ab6142e7a0e740123f46db
emgixiii <emgi@live.in>
2024-09-08 13:26:23 +0530

    DEL removed pastify

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -107,15 +107,6 @@ return {
     },
   },
   {
-    "TobinPalmer/pastify.nvim",
-    cmd = { "Pastify" },
-    config = function()
-      require("pastify").setup {
-        opts = {},
-      }
-    end,
-  },
-  {
     "gaoDean/autolist.nvim",
     ft = {
       "markdown",
