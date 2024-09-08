869b6d6902d2c76eb94a1a5ac1b5e40a6df6b783
emgixiii <emgi@live.in>
2024-09-08 07:17:36 +0530

    FIX edit with model 4o

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -24,6 +24,16 @@ return {
         top_p = 0.1,
         n = 1,
       },
+      openai_edit_params = {
+        model = "gpt-4o-mini",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 4095,
+        temperature = 0.2,
+        top_p = 0.1,
+        n = 1,
+      },
+
       actions_paths = { "~/.config/nvim/lua/plugins/chatgpt_actions.json" },
     }
   end,
