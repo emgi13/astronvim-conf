43be58c4645fe46758013d0b7823cd8b0388b9f6
emgixiii <emgi@live.in>
2024-10-18 12:16:45 +0530

    feat: fullscreen keymap neovide

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -12,14 +12,18 @@ return {
         neovide_transparency = 0.8,
         neovide_theme = "dark",
         neovide_detach_on_quit = "always_quit",
-        neovide_fullscreen = true,
         neovide_window_blurred = true,
+        neovide_fullscreen = true,
       },
     },
     mappings = {
       n = {
         [""] = { ":w<CR>" },
         [""] = { '"+P' },
+        ["<C-F11>"] = {
+          function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end,
+          desc = "Toggle Fullscreen",
+        },
       },
       v = {
         [""] = { '"+y' },
