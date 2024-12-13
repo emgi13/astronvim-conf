4e9754cdd8d691b31246e701f990e0fba2c5c5d0
emgixiii <emgi@live.in>
2024-12-13 12:21:07 +0530

    chore: update

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -25,17 +25,15 @@ return {
         n = 1,
       },
       openai_edit_params = {
-        model = "gpt-4o-mini",
+        model = "gpt-4o",
         frequency_penalty = 0,
         presence_penalty = 0,
-        max_tokens = 4095,
-        temperature = 0.2,
+        max_tokens = 12000,
         top_p = 0.1,
         n = 1,
       },
 
       ignore_default_actions_path = true,
-      actions_paths = { "~/.config/nvim/lua/plugins/chatgpt_actions.json" },
     }
   end,
   dependencies = {
