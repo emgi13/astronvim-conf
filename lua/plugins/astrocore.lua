05d6ea6ac8752844d17022bad81f542fc3daecbd
emgixiii <emgi@live.in>
2025-04-22 22:18:33 +0530

    fix: markdown preview command

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -80,7 +80,7 @@ return {
           end,
           desc = "Toggle Virt using Nabla",
         },
-        ["<Leader>M"] = { ":MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview`" },
+        ["<Leader>M"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
       },
