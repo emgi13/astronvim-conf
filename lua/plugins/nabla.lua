91028f5814964c235cd375f6a32a1678dd65a829
emgixiii <emgi@live.in>
2025-04-26 16:14:31 +0530

    fix: markdown render keymaps

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -10,15 +10,5 @@ return {
       function() require("nabla").popup() end,
       desc = "Preview MATH",
     },
-    {
-      "<Leader>uv",
-      function()
-        require("nabla").toggle_virt {
-          autogen = true, -- auto-regenerate ASCII art when exiting insert mode
-          silent = true, -- silents error messages
-        }
-      end,
-      desc = "Toggle Virtual Math",
-    },
   },
 }
--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,11 +1,7 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {
-    latex = { enabled = false }, -- Disables render-markdown's LaTeX
-    on = {
-      render = function() require("nabla").enable_virt { autogen = false } end,
-      clear = function() require("nabla").disable_virt() end,
-    },
+    latex = { enabled = false },
   },
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 }
