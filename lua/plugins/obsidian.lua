e93c8cff791ea30bc9387846962d34d97487507f
emgixiii <emgi@live.in>
2025-06-19 15:29:48 +0530

    fix: Normal file names

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -59,6 +59,26 @@ return {
         return out
       end,
 
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
+        -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          return title:gsub("[^A-Za-z0-9 ]", "")
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+          return tostring(os.time()) .. "-" .. suffix
+        end
+      end,
       -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
       -- URL it will be ignored but you can customize this behavior here.
       follow_url_func = vim.ui.open,
