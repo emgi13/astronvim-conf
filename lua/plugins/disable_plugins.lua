9af191610998d994bb94fb3dc5bc42441300bb03
emgixiii <emgi@live.in>
2025-07-01 19:04:19 +0530

    rem: unused plugins

---
+++ lua/plugins/disable_plugins.lua
@@ -0,0 +1,8 @@
+---@type LazySpec[]
+return {
+  { "https://github.com/max397574/better-escape.nvim", enabled = false }, -- Not required as i have remapped capslock to esc
+  { "https://github.com/windwp/nvim-ts-autotag", enabled = false }, -- This is messing with luasnip
+  { "https://github.com/s1n7ax/nvim-window-picker", enabled = false }, -- Window picker not used regularly
+  { "https://github.com/lervag/vimtex", enabled = false }, -- not required as telescope is available
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false }, -- using snacks picker instead
+}
