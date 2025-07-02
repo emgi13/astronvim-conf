d47095bd7a9af9321da539661b6c0573bb52ff46
emgixiii <emgi@live.in>
2025-07-02 18:35:17 +0530

    fix: load nabla only when markdown file

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -1,14 +1,15 @@
+---@type LazySpec
 return {
   "jbyuki/nabla.nvim",
   ft = {
     "markdown",
     "markdown.mdx",
   },
-  keys = {
-    {
-      "<Leader>m",
-      function() require("nabla").popup() end,
-      desc = "Preview MATH",
-    },
-  },
+  config = function()
+    require("astrocore").set_mappings {
+      n = {
+        ["<Leader>m"] = { function() require("nabla").popup() end, desc = "Popup Math" },
+      },
+    }
+  end,
 }
