64caf8219d8e7f1ab5549ca7e78bc2d76c34af51
emgixiii <emgi@live.in>
2025-07-02 17:14:50 +0530

    add: julia packs

--- lua/plugins/julials.lua
+++ lua/plugins/julials.lua
@@ -1,30 +1,20 @@
 return {
   "AstroNvim/astrolsp",
   opts = {
+    -- manually set up the Julia language server
+    -- since we won't be using Mason
     servers = { "julials" },
     config = {
       julials = {
         on_new_config = function(new_config)
           -- check for nvim-lspconfig julia sysimage shim
+          -- this path may need to be updated if you set it up differently
+          -- than the repository recommends above
           local julia = (vim.env.JULIA_DEPOT_PATH or vim.fn.expand "~/.julia")
             .. "/environments/nvim-lspconfig/bin/julia"
+          -- if the shim is found, then update the julia command
           if require("lspconfig").util.path.is_file(julia) then new_config.cmd[1] = julia end
         end,
-        -- recommended default settings used by Julia VS Code extension
-        settings = {
-          julia = {
-            completionmode = "qualify",
-            lint = {
-              missingrefs = "none",
-            },
-            inlayHints = {
-              static = {
-                enabled = false,
-                variableTypes = { enabled = true },
-              },
-            },
-          },
-        },
       },
     },
   },
