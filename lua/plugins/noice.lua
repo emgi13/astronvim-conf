69efb3dc478c9982623c41f1979b6e01a6e86816
emgixiii <emgi@live.in>
2025-07-02 18:36:24 +0530

    fix: disable file for noice.nvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,21 +1,21 @@
-return {}
+if true then return {} end -- WARN: Remove this line to enable this file.
 
--- return {
---   "folke/noice.nvim",
---   opts = {
---     lsp = {
---       signature = {
---         enabled = true,
---         view = "hover",
---       },
---       hover = {
---         enabled = true,
---         view = "hover",
---       },
---     },
---     presets = {
---       bottom_search = true,
---       command_palette = true,
---     },
---   },
--- }
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      signature = {
+        enabled = true,
+        view = "hover",
+      },
+      hover = {
+        enabled = true,
+        view = "hover",
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+    },
+  },
+}
