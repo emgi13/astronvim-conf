ad17e09c703b43f77231044d97cb6865371f9829
emgixiii <emgi@live.in>
2025-07-03 11:44:06 +0530

    feat: helix style whichkeyu

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  ---@type wk.Opts
+  opts = {
+    preset = "helix",
+  },
+  keys = {
+    {
+      "<leader>?",
+      function() require("which-key").show { global = false } end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
