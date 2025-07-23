86a19e9a3daa78454b4af847092cc775e729853e
emgixiii <emgi@live.in>
2025-07-23 19:08:39 +0530

    fix: local keymapping for nabla (markdown)

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -5,14 +5,31 @@ return {
     "markdown",
     "markdown.mdx",
   },
-  config = function()
-    require("astrocore").set_mappings {
-      n = {
-        ["<Leader>m"] = {
-          function() require("nabla").popup() end,
-          desc = "Popup Math",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        autocmds = {
+          buffer_enable_nabla = {
+            {
+              event = "FileType",
+              pattern = { "markdown", "markdown.mdx" },
+              desc = "Enable Nabla buffer local",
+              callback = function(opts)
+                require("astrocore").set_mappings({
+                  n = {
+                    ["<localleader>m"] = {
+                      function() require("nabla").popup() end,
+                      desc = "Preview Math",
+                    },
+                  },
+                }, { buffer = opts.buf })
+              end,
+            },
+          },
         },
       },
-    }
-  end,
+    },
+  },
 }
