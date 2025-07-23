fe711a2c8af5b5aad15dc0de2948bf994063490e
emgixiii <emgi@live.in>
2025-07-23 22:43:00 +0530

    fix: disable bufferlocal in whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -6,11 +6,4 @@ return {
   opts = {
     preset = "helix",
   },
-  keys = {
-    {
-      "<leader>?",
-      function() require("which-key").show { global = false } end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
 }
