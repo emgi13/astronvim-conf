7bda6dfd4dabd80febafed235a33b787738aec9c
emgixiii <emgi@live.in>
2025-08-06 23:34:52 +0530

    feat: change picker style to max screen space

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -64,6 +64,25 @@ return {
         frecency = true,
         cwd_bonus = true,
       },
+      layout = {
+        preview = "main",
+        layout = {
+          box = "vertical",
+          backdrop = false,
+          width = 0,
+          height = 0.2,
+          position = "bottom",
+          border = "top",
+          title = " {title} {live} {flags}",
+          title_pos = "left",
+          { win = "input", height = 1, border = "bottom" },
+          {
+            box = "horizontal",
+            { win = "list", border = "none" },
+            { win = "preview", title = "{preview}", width = 0.6, border = "left" },
+          },
+        },
+      },
     },
   },
   dependencies = {
