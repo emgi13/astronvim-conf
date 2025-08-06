058d5adf45e9ab85c73b047751e7b6c0d2c940da
emgixiii <emgi@live.in>
2025-08-07 00:13:07 +0530

    fix: make snacks picker use delta pager

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -60,6 +60,22 @@ return {
       },
     },
     picker = {
+      previewers = {
+        diff = {
+          builtin = false, -- use Neovim for previewing diffs (true) or use an external tool (false)
+          cmd = { "delta" }, -- example to show a diff with delta
+        },
+        git = {
+          builtin = false, -- use Neovim for previewing git output (true) or use git (false)
+          args = { "-c", "core.pager=delta" }, -- additional arguments passed to the git command. Useful to set pager options usin `-c ...`
+        },
+        -- file = {
+        --   max_size = 1024 * 1024, -- 1MB
+        --   max_line_length = 500, -- max line length
+        --   ft = nil, ---@type string? filetype for highlighting. Use `nil` for auto detect
+        -- },
+        -- man_pager = nil, ---@type string? MANPAGER env to use for `man` preview
+      },
       matcher = {
         frecency = true,
         cwd_bonus = true,
