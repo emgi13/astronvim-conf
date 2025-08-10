b47a01f37ba08ec1eac7da98197bef7b0bb48f34
emgixiii <emgi@live.in>
2025-08-10 23:29:04 +0530

    fix: consistency in disabled packages

--- lua/plugins/disable_plugins.lua
+++ lua/plugins/disable_plugins.lua
@@ -1,8 +1,8 @@
 ---@type LazySpec[]
 return {
-  { "https://github.com/max397574/better-escape.nvim", enabled = false }, -- Not required as i have remapped capslock to esc
-  { "https://github.com/windwp/nvim-ts-autotag", enabled = false }, -- This is messing with luasnip
-  { "https://github.com/s1n7ax/nvim-window-picker", enabled = false }, -- Window picker not used regularly
+  { "max397574/better-escape.nvim", enabled = false }, -- Not required as i have remapped capslock to esc
+  { "windwp/nvim-ts-autotag", enabled = false }, -- This is messing with luasnip
+  { "s1n7ax/nvim-window-picker", enabled = false }, -- Window picker not used regularly
   { "nvim-neo-tree/neo-tree.nvim", enabled = false }, -- using snacks picker instead
   { "nvim-telescope/telescope.nvim", enabled = false }, -- using snacks picker instead,
 }
