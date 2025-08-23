5868a847563c45c932ca30afc189a72a972d1e0b
emgixiii <emgi@live.in>
2025-08-23 16:05:08 +0530

    fix: reenable autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,10 +1,5 @@
---- INFO: Disbale in Markdown (Conflicting with Obsidian)
----
 ---@type LazySpec
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  opts = {
-    disable_filetype = { "markdown" },
-  },
 }
