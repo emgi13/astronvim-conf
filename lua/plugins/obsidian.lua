0933864ec915307f943442bd199284c6572341ab
emgixiii <emgi@live.in>
2025-08-24 16:33:54 +0530

    fix: obsidian disable frontmatter func

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -80,18 +80,18 @@ return {
         min_chars = 0, -- WARN: start completion instantly, increase if slows down
       },
 
-      note_frontmatter_func = function(note)
-        -- This is equivalent to the default frontmatter function.
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-        return out
-      end,
+      -- note_frontmatter_func = function(note)
+      --   -- This is equivalent to the default frontmatter function.
+      --   local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      --   -- `note.metadata` contains any manually added fields in the frontmatter.
+      --   -- So here we just make sure those fields are kept in the frontmatter.
+      --   if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+      --     for k, v in pairs(note.metadata) do
+      --       out[k] = v
+      --     end
+      --   end
+      --   return out
+      -- end,
 
       -- Optional, customize how note IDs are generated given an optional title.
       ---@param title string|?
