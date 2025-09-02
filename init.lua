3ecabb2611bb17682ccb5596bb3fce822ab527ae
emgixiii <emgi@live.in>
2025-09-03 00:19:38 +0530

    fix: set clipboard to win32yank

--- init.lua
+++ init.lua
@@ -16,14 +16,14 @@ if not pcall(require, "lazy") then
 end
 
 vim.g.clipboard = {
-  name = "WslClipboard",
+  name = "Win32Yank",
   copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
   },
   paste = {
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
   },
   cache_enabled = 0,
 }
