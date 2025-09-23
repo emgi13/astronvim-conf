f64bf6e9161a01a756bdbdc1608c94ab2fa9e74d
emgixiii <emgi@live.in>
2025-09-23 22:55:32 +0530

    fix: virt with nabla

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -4,6 +4,7 @@ return {
   ft = { "markdown", "markdown.mdx" },
   opts = {
     latex = { enabled = false },
+    win_options = { conceallevel = { rendered = 2 } },
   },
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 }
