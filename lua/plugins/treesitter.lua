10c942eba0986a5c35f717212c209ccac010556d
emgixiii <emgi@live.in>
2026-03-09 10:39:15 +0530

    fix: tree-sitter cli path needs to be hardcoded

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,10 +31,10 @@ return {
     local wrapper_path = data_path .. "/tree-sitter"
     local f = io.open(wrapper_path, "w")
     if f then
-      f:write("#!/bin/bash\n")
-      f:write('args=()\n')
-      f:write('for arg in "$@"; do [[ "$arg" != "--no-bindings" ]] && args+=("$arg"); done\n')
-      f:write('exec /home/emgixiii/.cargo/bin/tree-sitter "${args[@]}"\n')
+      f:write "#!/bin/bash\n"
+      f:write "args=()\n"
+      f:write 'for arg in "$@"; do [[ "$arg" != "--no-bindings" ]] && args+=("$arg"); done\n'
+      f:write 'exec /home/linuxbrew/.linuxbrew/bin/tree-sitter "${args[@]}"\n'
       f:close()
       vim.fn.setfperm(wrapper_path, "rwxr-xr-x")
       -- Put our wrapper at the front of the PATH for this Neovim session
