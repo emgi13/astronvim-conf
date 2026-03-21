a1c64a6f5ddf652fb60dc510dd5b660c276372e8
emgixiii <emgi@live.in>
2026-03-21 12:45:25 +0530

    ref: harper_ls into its own file

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -72,6 +72,17 @@ return {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
       harper_ls = {
         autostart = false,
+        settings = {
+          ["harper-ls"] = {
+            codeActions = {
+              forceStable = true,
+            },
+            linters = {
+              spelled_numbers = true,
+              linking_verbs = true,
+            },
+          },
+        },
       },
       basedpyright = {
         settings = {
---
+++ lua/plugins/harper.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "harper_ls" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "harper-ls" })
+    end,
+  },
+}
