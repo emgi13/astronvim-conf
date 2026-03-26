0be3bb6500c127be0ded5bb4bbb8307fa2bdf5da
emgixiii <emgi@live.in>
2026-03-26 14:45:40 +0530

    switch treesitter to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
   opts = {
     ensure_installed = {
       "lua",
@@ -31,10 +33,10 @@ return {
     local wrapper_path = data_path .. "/tree-sitter"
     local f = io.open(wrapper_path, "w")
     if f then
-      f:write("#!/bin/bash\n")
-      f:write('args=()\n')
-      f:write('for arg in "$@"; do [[ "$arg" != "--no-bindings" ]] && args+=("$arg"); done\n')
-      f:write('exec /home/emgixiii/.cargo/bin/tree-sitter "${args[@]}"\n')
+      f:write "#!/bin/bash\n"
+      f:write "args=()\n"
+      f:write 'for arg in "$@"; do [[ "$arg" != "--no-bindings" ]] && args+=("$arg"); done\n'
+      f:write 'exec /home/emgixiii/.cargo/bin/tree-sitter "${args[@]}"\n'
       f:close()
       vim.fn.setfperm(wrapper_path, "rwxr-xr-x")
       -- Put our wrapper at the front of the PATH for this Neovim session
