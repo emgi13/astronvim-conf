660e05dd131a6b4fbd8f3453c2aa5733a84da8d1
emgixiii <emgi@live.in>
2026-04-02 18:18:51 +0530

    fix: move wsl specific vimtex args to env file

--- lua/env_setup.lua
+++ lua/env_setup.lua
@@ -5,4 +5,6 @@ if is_wsl then
   vim.g.node_host_prog = "/home/linuxbrew/.linuxbrew/bin/neovim-node-host"
   vim.g.ruby_host_prog = "/home/linuxbrew/.linuxbrew/lib/ruby/gems/3.4.0/bin/neovim-ruby-host"
   vim.g.perl_host_prog = "PERL5LIB=/home/emgixiii/perl5/lib/perl5 /usr/bin/perl"
+  vim.g.vimtex_view_sioyek_exe = "sioyek.exe" -- Windows exe in PATH
+  vim.g.vimtex_callback_progpath = "wsl nvim" -- Call nvim inside WSL from Windows inverse search, also enable `vimtex_wsl_fix 1` in sioyek
 end
